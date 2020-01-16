f4765048536be9b3ca2eb5688ea55a53b3e1dcfb
Artem Kochuev <kochuev.a@yandex.ru>
2020-01-17 01:57:32 +0300

    small fix of depends

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-import layout from '../css/layout/layout.css';
-import themeJs from '../css/theme/theme.js'
+import layout from '../source/layout/layout.css';
+import themeJs from '../source/theme/theme.js.js'
